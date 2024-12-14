f1444785d694bc4697f42724dd04b10e09eb35b6
euleovargas <contato@leovargas.com.br>
2024-12-14 18:04:17 -0300

    fix: reorganiza declarações de tipos do NextAuth

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,24 +1,20 @@
 import { UserRole } from "@prisma/client"
 import NextAuth, { DefaultSession } from "next-auth"
 
+interface LunaUser {
+  id: string
+  name?: string | null
+  email?: string | null
+  image?: string | null
+  role: UserRole
+}
+
 declare module "next-auth" {
   interface Session {
-    user: {
-      id: string
-      name?: string | null
-      email?: string | null
-      image?: string | null
-      role: UserRole
-    }
+    user: LunaUser
   }
 
-  interface User {
-    id: string
-    name?: string | null
-    email?: string | null
-    image?: string | null
-    role: UserRole
-  }
+  interface User extends LunaUser {}
 }
 
 declare module "next-auth/jwt" {
