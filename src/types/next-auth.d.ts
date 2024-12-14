b8535e2d99d178111779465bb8e94e4e0ae39962
euleovargas <contato@leovargas.com.br>
2024-12-14 04:04:33 -0300

    refactor: adiciona tipo CustomSession para resolver problemas de tipagem

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,16 +1,14 @@
 import { UserRole } from "@prisma/client"
-import NextAuth, { DefaultSession } from "next-auth"
+import NextAuth from "next-auth"
 
 declare module "next-auth" {
   interface User {
-    id: string
-    role: UserRole
+    role?: UserRole
   }
 }
 
 declare module "next-auth/jwt" {
   interface JWT {
-    id: string
-    role: UserRole
+    role?: UserRole
   }
 }
