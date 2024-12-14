774b6dfea6479b38454bdffc2786bd5477cde4a7
euleovargas <contato@leovargas.com.br>
2024-12-14 03:50:09 -0300

    fix: adiciona tipagem personalizada para User do next-auth

---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,16 @@
+import { UserRole } from "@prisma/client";
+import NextAuth from "next-auth"
+
+declare module "next-auth" {
+  interface User {
+    id: string
+    name?: string | null
+    email?: string | null
+    image?: string | null
+    role: UserRole
+  }
+
+  interface Session {
+    user: User
+  }
+}
