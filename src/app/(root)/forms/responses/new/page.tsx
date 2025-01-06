f7fae2feb019fcc1591e702a1f435d0d355fef44
euleovargas <contato@leovargas.com.br>
2025-01-05 21:38:30 -0300

    fix: update response-form import path

--- src/app/(root)/forms/responses/new/page.tsx
+++ src/app/(root)/forms/responses/new/page.tsx
@@ -3,7 +3,7 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
 import { redirect } from "next/navigation";
 import prisma from "@/lib/prisma";
-import { ResponseForm } from "@/app/forms/responses/[responseId]/response-form";
+import { ResponseForm } from "@/app/(root)/forms/responses/[responseId]/response-form";
 
 export const metadata: Metadata = {
   title: "Preencher Formulário",
